a848354100a42f55435f81c6a366f8f3bf1eaa55
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-10-01 15:28:00 +0530

    chore(data): Initialize sample listings in init/data.js

--- app.js
+++ app.js
@@ -13,20 +13,6 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-const sampleListing = new Listing({
-  title: "Jumeirah Burj Al Arab",
-  description:
-    "Duplex One Bedroom Suite, Complimentary Access to Wild Wadi Waterpark",
-  image:
-    "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  price: 45554,
-  location: "733 Beatty Street, Vancouver, British Columbia V6B 2M4",
-  country: "Canada",
-});
-
-sampleListing.save().then((res) => {
-  console.log(res);
-});
 
 app.get("/", (req, res) => {
   res.send("Home Page");
