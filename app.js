c9a975f33749368ad2ec68f38e5d69d5db146d73
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-10-01 21:59:15 +0530

    feat(routes): Add DELETE /listings/:id route to remove listing using findByIdAndDelete

--- app.js
+++ app.js
@@ -73,6 +73,13 @@ app.put("/listings/:id", async (req, res) => {
   res.redirect(`/listings/${id}`);
 });
 
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
+
 app.listen(8080, () => {
   console.log("Server is listening on port 8080");
 });
