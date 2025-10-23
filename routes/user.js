8a92dddc2f00363edcba9258f1893c125231d2b1
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-10-23 21:27:57 +0530

    feat(auth): Added GET and POST routes in user.js to render and process login/signup forms

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
+const passport = require("passport");
+
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
@@ -18,5 +20,21 @@ router.post("/signup", async (req, res) => {
     res.redirect("/signup");
   }
 });
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome Back to Travii");
+    res.redirect("/listings");
+  }
+);
 
 module.exports = router;
