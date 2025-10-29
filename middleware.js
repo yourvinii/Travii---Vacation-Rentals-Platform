c28bcb11d8c9ce4c496f6cbbe632c8fbc289c14d
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-10-29 22:48:42 +0530

    feat(auth): Added middleware.js to restrict access and flash login prompt for unauthenticated users

---
+++ middleware.js
@@ -0,0 +1,19 @@
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.session.redirectUrl = req.originalUrl;
+    // console.log(req);
+    req.flash(
+      "error",
+      "⚠️ Please log in to continue. You need an account to access this service."
+    );
+    return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
