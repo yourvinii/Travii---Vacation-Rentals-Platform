037f2c3cb8587938e01aad69a70511305b3386f6
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-11-01 21:18:43 +0530

    refactor(MVC): Added controllers/users.js and organized user routes for cleaner authentication and modular structure

---
+++ controller/users.js
@@ -0,0 +1,47 @@
+const User = require("../models/user.js");
+
+module.exports.renderSignUpForm = (req, res) => {
+  res.render("users/signup.ejs");
+};
+
+module.exports.signup = async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    const newUser = new User({ email, username });
+    const registerdUser = await User.register(newUser, password);
+    req.logIn(registerdUser, (err) => {
+      if (err) {
+        return next();
+      }
+      console.log(registerdUser);
+      req.flash("success", "Welcome to Travii");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+};
+
+module.exports.renderloginForm = (req, res) => {
+  res.render("users/login.ejs");
+};
+
+module.exports.login = async (req, res) => {
+  req.flash("success", "Welcome Back to Travii");
+  let redirectUrl = res.locals.redirectUrl || "/listings";
+  res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash(
+      "success",
+      "🌟 You’re logged out! Come back anytime — we’ll be here."
+    );
+    res.redirect("/listings");
+  });
+};
