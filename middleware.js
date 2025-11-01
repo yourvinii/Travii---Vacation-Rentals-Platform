e758243ef0100fcfbb298131a277f79db2d5b8cc
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-11-01 16:58:18 +0530

    refactor(middleware): added isOwner, isReviewAuthor and moved validation logic for better modularity and security

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review");
+const ExpressErr = require("./utils/ExpressErr");
+const { listingSchema, reviewSchema } = require("./schema.js");
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
@@ -14,6 +19,46 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.saveRedirectUrl = (req, res, next) => {
   if (req.session.redirectUrl) {
     res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
+
+module.exports.isOwner = async (req, res, next) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (!listing.owner.equals(res.locals.currentUser._id)) {
+    req.flash("error", "⚠️ You don’t have permission to modify this listing.");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
+
+module.exports.validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressErr(400, error);
+  } else {
+    next();
+  }
+};
+
+module.exports.validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressErr(400, error);
+  } else {
+    next();
+  }
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  let { id, reviewId } = req.params;
+  let review = await Review.findById(reviewId);
+  if (!review.author.equals(res.locals.currentUser._id)) {
+    req.flash("error", "⚠️ You don’t have permission to delete this review.");
+    return res.redirect(`/listings/${id}`);
   }
   next();
 };
