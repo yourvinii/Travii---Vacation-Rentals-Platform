14cd19b17303bb1dccf3aed8d63525daef7a07b4
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-11-01 16:34:33 +0530

    chore(seed): Updated initData to assign owner field to each listing for user association

--- init/index.js
+++ init/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/travii";
 
 main().then(() => {
@@ -14,6 +15,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({ ...obj, owner: "68f9d1f0965413d26612d851" }));
   await Listing.insertMany(initData.data);
   console.log("Data was Initialized ");
 };
