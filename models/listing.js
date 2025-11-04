f7635f744c82a3ce93c3e4a146f68827a8d3326a
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-11-04 14:10:29 +0530

    refactor(model): Updated Listing schema to store image as object with url and filename for Cloudinary integration

--- models/listing.js
+++ models/listing.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
 const User = require("./user.js");
+const { string } = require("joi");
 
 const listingSchema = new Schema({
   title: {
@@ -10,13 +11,8 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1655439002732-ef4fdb3050b8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1655439002732-ef4fdb3050b8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        : v,
+    url: String,
+    filename: String,
   },
   price: Number,
   location: String,
