c44f2b4d8fc6aed984f242ff98db732b504d525a
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2025-11-15 18:37:28 +0530

    feat(navbar): Added onclick handlers in script.js for interactive menu actions

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,32 @@
-
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-  'use strict'
+  "use strict";
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+  const forms = document.querySelectorAll(".needs-validation");
 
   // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+// NavBar
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+});
